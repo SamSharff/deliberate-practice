e6d1070b0f59bc0daacc0aa5ff077625da06167f
Sam Sharff <samanthaleec@gmail.com>
2022-06-06 15:05:25 -0400

    practice with whiteboarding cheatsheet, array_max.rb

---
+++ TIP/array_max.rb
@@ -0,0 +1,30 @@
+# Write a function that returns the greatest value from an array of numbers.
+
+# Input: [5, 17, -4, 20, 12]
+# Output: 20
+
+# Input: []
+# Output: "No value given"
+
+# Input: [1, 2, 3, 4, 4]
+# Output: 4
+
+# FOCUS
+# while loop and each loop
+#   while loop is most flexible
+#     con: manage your own index, i = 0
+#   each loop --> popular in working environment, you know you need to go through each element in the array 
+#   map loop --> also popular; go through every element and, for example, add 1 (don't worry for whiteboarding)
+
+# Visualize
+# create a function
+# create a loop (while here to start)
+# start with index 0
+# create variable max, set to array[0]
+# look at next number and compare to variable max. if that number is larger, set variable max to that new number; if it's equal to, just skip and keep going through array until you get to the last number.
+# if empty array, return "No value given"
+# after loop complete, return variable max
+# print sample cases when testing to show edge cases work
+
+
+
