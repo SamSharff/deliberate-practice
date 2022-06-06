d994ae7491d5df51e37c11bd3663ea051cab950d
Sam Sharff <samanthaleec@gmail.com>
2022-06-06 15:40:25 -0400

    finished array_max using whiteboarding cheat sheet

--- TIP/array_max.rb
+++ TIP/array_max.rb
@@ -16,7 +16,7 @@
 #   each loop --> popular in working environment, you know you need to go through each element in the array 
 #   map loop --> also popular; go through every element and, for example, add 1 (don't worry for whiteboarding)
 
-# Visualize
+# VISUALIZE #
 # create a function
 # create a loop (while here to start)
 # start with index 0
@@ -26,5 +26,27 @@
 # after loop complete, return variable max
 # print sample cases when testing to show edge cases work
 
+# CODE #
+def max_number(array)
+  greatest_number = array[0]         #1 gn = 5
+  index = 0                          #2 index = 0
+  
+  while index < array.length
+    current_number = array[index]    #3 cn = 5  
+    if greatest_number < current_number
+      greatest_number = current_number
+    end   
+    index = index + 1                #4 index = 1
+  end
 
+  if array == nil             
+    p "No value given"                #Note: this is not working correctly. How to print test instead of "nil"?
+  end
+
+  return greatest_number
+end
+
+p max_number([5, 17, -4, 20, 12])
+p max_number([])
+p max_number([1, 2, 3, 4, 4])
 
