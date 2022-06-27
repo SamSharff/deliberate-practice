39301605befaa84d565ea2d1b22acbea9c6fd36f
Sam Sharff <samanthaleec@gmail.com>
2022-06-27 17:01:10 -0400

    adds loop/array printing review

--- ruby-review/exercises_ruby_review.rb
+++ ruby-review/exercises_ruby_review.rb
@@ -1,9 +1,33 @@
-# Create a program that asks the user for their favorite 5 foods. Then display those foods as an array, using the p keyword.
+# 1.  Create a program that asks the user for their favorite 5 foods. Then display those foods as an array, using the p keyword.
 
-array = []
-puts "please enter five of your favorite foods"
+# array = []
+# puts "please enter five of your favorite foods"
+# 5.times do
+#   food = gets.chomp
+#   array << food
+# end
+# p array
+
+# 2. Now, instead of printing out the array, output 5 separate lines of each food, with the prefix, “I love”. For example:
+
+# I love cobb salad
+# I love spaghetti
+# I love pizza
+# I love Swedish meatballs
+# I love mac and cheese
+
+favorite_foods = []
+puts "please enter your five favorite foods"
 5.times do
   food = gets.chomp
-  array << food
+  favorite_foods << food
 end
-p array
+
+index = 0
+while index < favorite_foods.length
+  p "I love #{favorite_foods[index]}."
+  index += 1
+end
+
+
+
