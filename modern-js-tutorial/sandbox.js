11ebe20570171c39b92aa7f9b80ba97417f72776
Sam Sharff <samanthaleec@gmail.com>
2022-09-01 17:33:59 -0400

    brush up on let vs const vs var

--- modern-js-tutorial/sandbox.js
+++ modern-js-tutorial/sandbox.js
@@ -1,1 +1,21 @@
-alert("hello world");
+// let
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+// const
+const points = 100;
+console.log(points);
+
+// *** won't work *** //
+// points = 50;
+// console.log(points);
+// *** won't work *** //
+
+// var
+var score = 75;
+console.log(score);
