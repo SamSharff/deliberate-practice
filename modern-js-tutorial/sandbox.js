5ff35cbd81f524fec3bbbf76f2d93e3cad9074e3
Sam Sharff <samanthaleec@gmail.com>
2022-09-02 09:29:22 -0400

    review of JS basics, methods vs functions, syntax for methods

--- modern-js-tutorial/sandbox.js
+++ modern-js-tutorial/sandbox.js
@@ -31,10 +31,18 @@ let firstName = "Samantha";
 let lastName = "Sharff";
 
 let fullName = firstName + " " + lastName;
-console.log(fullName);
 
 // getting characters
+console.log(fullName[2]);
 
 // string length
+console.log(fullName.length);
 
 // string methods
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf("@");
+console.log(index);
