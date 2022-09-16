b9c6b093f52f6c62acaf3263a283ce18a31219f6
Sam Sharff <samanthaleec@gmail.com>
2022-09-16 14:58:08 -0400

    long and short hand examles; NaN

--- modern-js-tutorial/sandbox.js
+++ modern-js-tutorial/sandbox.js
@@ -1,13 +1,33 @@
-//*** common string methods ***//
+let radius = 10;
+const pi = 3.14;
 
-let email = "sam@example.com";
+// console.log(radius, pi);
 
-// let result = email.lastIndexOf("e");
+// math operators +, -, *, ?, **, %
 
-// let result = email.slice(2, 3);
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius ** 2;
 
-// let result = email.substr(3, 7); (starting index, characters to retrieve)
-// note: substr is now deprecated
+// order of operations - B I D M A S
 
-// let result = email.replace("s", "l");
-console.log(result);
+// let result = 5 * (10 - 3) ** 2;
+
+// console.log(result); // 245
+
+let likes = 10;
+//LONG HAND//
+// likes = likes + 1;
+//SHORT HAND//
+// likes += 1;
+// likes++;
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
+
+// console.log(likes);
+
+// NaN - not a number
+console.log("hello" / 5);
+console.log("hello" * 5);
