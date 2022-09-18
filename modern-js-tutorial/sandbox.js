6861b3009a4d90c87153f5f882992a188ddba155
Sam Sharff <samanthaleec@gmail.com>
2022-09-18 09:04:21 -0400

    reviews .join, .indexOf, .concat, .push, and .pop methods

--- modern-js-tutorial/sandbox.js
+++ modern-js-tutorial/sandbox.js
@@ -5,8 +5,22 @@ let ninjas = ["shaun", "ryu", "chun-li"];
 // let ages = [20, 35, 30, 35];
 // console.log(ages[2]);
 
-let random = ["shaun", "crystal", 30, 20];
-console.log(random);
+// let random = ["shaun", "crystal", 30, 20];
+// console.log(random);
 
-const name = null ?? "Sam";
-console.log(name);
+// const name = null ?? "Sam";
+// console.log(name);
+
+// console.log(ninjas.length);
+
+// array methods //
+
+// let result = ninjas.join(" - ");
+// let result = ninjas.indexOf("ryu");
+// let result = ninjas.concat("Evelyn", "Aaron");
+// push alters original value (destructive method) //
+let result = ninjas.push("Uni");
+console.log(ninjas);
+result = ninjas.pop();
+console.log(ninjas);
+console.log(result);
