4a649a22fc5d4546dc649dbd4b5229c32fbba892
Sam Sharff <samanthaleec@gmail.com>
2022-10-11 11:28:02 -0400

    adds solution to palindrome problem

--- TIP/palindrome.rb
+++ TIP/palindrome.rb
@@ -1,8 +1,33 @@
+# Given an integer x, return true if x is palindrome integer.
+
+# An integer is a palindrome when it reads the same backward as forward.
+
+# set up function, call
+# argument, x
+# convert x to string
+# set variable "reverse" to x
+# reverse that variable
+# if reverse == x p true
+
 def is_palindrome(x)
-  return x.to_s.reverse
+  x = x.to_s                          #1 x = "123"
+  reversed_x = x                      #2 reversed_x = "123"
+  reversed_x = reversed_x.reverse        #3 reversed_x = "321"
+  if x == reversed_x
+    p true
+  else
+    p false
+  end
 end
 
-p is_palindrome(123)
-# output "321"
+# is_palindrome(121)
+# output false
 
-p is_palindrome()
+# is_palindrome(4221)
+#output false
+
+# is_palindrome(567765)
+# #output true
+
+is_palindrome(-567765)
+#output true
